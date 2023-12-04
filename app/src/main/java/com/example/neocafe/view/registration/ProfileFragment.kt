36d4b5d91ffcf79651b7145ec20ef3322cbdfa8c
rybakova-auca-2021 <rybakova_k@auca.kg>
2023-12-04 23:54:22 +0600

    added profile fragment

---
+++ app/src/main/java/com/example/neocafe/view/registration/ProfileFragment.kt
@@ -0,0 +1,34 @@
+package com.example.neocafe.view.registration
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.neocafe.R
+import com.example.neocafe.databinding.FragmentProfileBinding
+
+class ProfileFragment : Fragment() {
+    private lateinit var binding: FragmentProfileBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupNavigation()
+    }
+
+    private fun setupNavigation() {
+        binding.btnRegister.setOnClickListener {
+            val bottomSheetFragment = RegisterFragment()
+            bottomSheetFragment.show(parentFragmentManager, bottomSheetFragment.tag)
+        }
+    }
+
+}

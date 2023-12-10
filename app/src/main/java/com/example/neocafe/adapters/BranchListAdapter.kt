a77ee4477632ce99de7391bd1f7d24f2124bf4df
rybakova-auca-2021 <rybakova_k@auca.kg>
2023-12-10 17:43:43 +0600

    work on branch pages

---
+++ app/src/main/java/com/example/neocafe/adapters/BranchListAdapter.kt
@@ -0,0 +1,95 @@
+package com.example.neocafe.adapters
+
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.neocafe.databinding.CardBranchBinding
+import com.example.neocafe.model.Branch
+
+class BranchListAdapter(private var branches: List<Branch>) :
+    RecyclerView.Adapter<BranchListAdapter.ArticleViewHolder>() {
+
+    private var itemClickListener: OnItemClickListener? = null
+
+    fun setOnItemClickListener(listener: OnItemClickListener) {
+        itemClickListener = listener
+    }
+
+    interface OnItemClickListener {
+        fun onItemClick(branch: Branch)
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticleViewHolder {
+        val binding = CardBranchBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return ArticleViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ArticleViewHolder, position: Int) {
+        val branch = branches[position]
+        holder.bind(branch)
+    }
+
+    override fun getItemCount(): Int {
+        return branches.size
+    }
+
+    fun updateData(newList: List<Branch>) {
+        val diffResult = DiffUtil.calculateDiff(
+            ProductDiffCallback(
+                branches,
+                newList
+            )
+        )
+        branches = newList
+        diffResult.dispatchUpdatesTo(this)
+    }
+
+    inner class ArticleViewHolder(private val binding: CardBranchBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        init {
+            binding.root.setOnClickListener {
+                val position = adapterPosition
+                if (position != RecyclerView.NO_POSITION) {
+                    val clickedItem = branches[position]
+                    itemClickListener?.onItemClick(clickedItem)
+                }
+            }
+        }
+
+        fun bind(branch: Branch) {
+//            Glide.with(itemView.context)
+//                .load(promotion.image)
+//                .apply(
+//                    RequestOptions().transform(
+//                        CenterCrop(), // Optional: CenterCrop the image if needed
+//                        RoundedCorners(10) // Set the corner radius in pixels
+//                    )
+//                )
+//                .into(binding.branchImg)
+            binding.branchTitle.text = branch.title
+            binding.branchAddress.text = branch.address
+            binding.branchTime.text = "${branch.start_time}-${branch.end_time}"
+        }
+    }
+
+    class ProductDiffCallback(
+        private val oldList: List<Branch>,
+        private val newList: List<Branch>
+    ) : DiffUtil.Callback() {
+
+        override fun getOldListSize() = oldList.size
+
+        override fun getNewListSize() = newList.size
+
+        override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int) =
+            oldList[oldItemPosition].title == newList[newItemPosition].title
+
+        override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int) =
+            oldList[oldItemPosition] == newList[newItemPosition]
+    }
+}
--- app/src/main/java/com/example/neocafe/view/mainPage/AllBranchesFragment.kt
+++ app/src/main/java/com/example/neocafe/view/mainPage/AllBranchesFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.neocafe.R
 import com.example.neocafe.adapters.ViewPagerAdapter
 import com.example.neocafe.databinding.FragmentAllBranchesBinding
@@ -38,7 +39,9 @@ class AllBranchesFragment : Fragment() {
     }
 
     private fun setupNavigation() {
-        // Implement your navigation setup here
+        binding.btnBack.setOnClickListener {
+            findNavController().navigate(R.id.mainPageFragment)
+        }
     }
 }
 

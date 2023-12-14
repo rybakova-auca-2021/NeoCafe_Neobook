d9047cf3557591c0f6098e8192247fd007d46efd
rybakova-auca-2021 <rybakova_k@auca.kg>
2023-12-15 00:18:14 +0600

    created ui for order payments

---
+++ app/src/main/java/com/example/neocafe/view/basket/unregisteredUser/OrderPaymentFragment.kt
@@ -0,0 +1,22 @@
+package com.example.neocafe.view.basket.unregisteredUser
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.neocafe.R
+import com.example.neocafe.databinding.FragmentOrderPaymentBinding
+
+
+class OrderPaymentFragment : Fragment() {
+    private lateinit var binding: FragmentOrderPaymentBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentOrderPaymentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
---
+++ app/src/main/java/com/example/neocafe/view/basket/unregisteredUser/OrderPhoneFragment.kt
@@ -0,0 +1,19 @@
+package com.example.neocafe.view.basket.unregisteredUser
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.neocafe.R
+
+class OrderPhoneFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_order_phone, container, false)
+    }
+}

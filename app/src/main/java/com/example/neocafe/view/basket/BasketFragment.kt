8e3f422d9c1adff150c5335732f2105db4da08b6
rybakova-auca-2021 <rybakova_k@auca.kg>
2023-12-20 00:21:29 +0600

    added dialog for deleting items from basket

--- app/src/main/java/com/example/neocafe/view/basket/BasketFragment.kt
+++ app/src/main/java/com/example/neocafe/view/basket/BasketFragment.kt
@@ -1,15 +1,20 @@
 package com.example.neocafe.view.basket
 
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.navigation.fragment.findNavController
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.example.neocafe.R
 import com.example.neocafe.adapters.ViewPagerAdapter
 import com.example.neocafe.adapters.ViewPagerAdapterBasket
+import com.example.neocafe.constants.Utils
 import com.example.neocafe.databinding.FragmentBasketBinding
 import com.example.neocafe.room.MyApplication
 import com.example.neocafe.room.ProductDao
@@ -53,7 +58,7 @@ class BasketFragment : Fragment() {
 
     private fun setupNavigation() {
         binding.btnClear.setOnClickListener {
-            clearBasket()
+            showDialog()
         }
     }
 
@@ -80,6 +85,31 @@ class BasketFragment : Fragment() {
                     binding.btnClear.visibility = View.GONE
                 }
             }
+        }
+    }
+
+    private fun showDialog() {
+        val dialogView = LayoutInflater.from(requireContext()).inflate(R.layout.dialog_basket_clear, null)
+        val myDialog = Dialog(requireContext())
+        myDialog.setContentView(dialogView)
+
+        myDialog.setCancelable(true)
+        myDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        myDialog.show()
+
+        val closeBtn = myDialog.findViewById<TextView>(R.id.btn_cancel_clear)
+        val clearBtn = myDialog.findViewById<TextView>(R.id.btn_clear)
+
+        closeBtn.setOnClickListener {
+            myDialog.dismiss()
+        }
+        clearBtn.setOnClickListener {
+            myDialog.dismiss()
+            clearBasket()
+        }
+
+        dialogView.setOnClickListener {
+            myDialog.dismiss()
         }
     }
 }
